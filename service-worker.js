0faae1dfc6b54175cf5439b13c748c1dd51d20c2
caljun <calderonjunya0602@gmail.com>
2025-05-02 23:22:44 +0900

    g

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,50 @@
+const CACHE_NAME = 'now-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/mypage.html',
+  '/add-friend.html',
+  '/area-setup.html',
+  '/css/style.css',
+  '/css/mypage.css',
+  '/js/mypage.js',
+  '/js/add-friend.js',
+  '/js/area-setup.js',
+  '/utils/map.js',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js'
+];
+
+// インストール時にキャッシュ
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-  });
-  
+  console.log('[ServiceWorker] インストール');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 有効化イベント
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] 有効化');
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// fetch時はキャッシュ優先
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
